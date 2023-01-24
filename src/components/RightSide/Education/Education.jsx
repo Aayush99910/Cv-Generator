57efbd844d1718d80b19c0c2ccbfa6464c89d63b
Aayush99910 <amaharjann01@gmail.com>
2023-01-23 19:05:28 -0800

    User can now add or delete their degree

--- src/components/RightSide/Education/Education.jsx
+++ src/components/RightSide/Education/Education.jsx
@@ -1,39 +1,176 @@
 import React, { Component } from "react";
 import EducationCard from "./EducationCard";
 import "./Education.css";
-
+import {IoMdAddCircle} from 'react-icons/io';
+import {MdCancel} from "react-icons/md";
 
 export default class Education extends Component {
-    render() {
-        const educations = [
+    state = {
+        isEditing: false,
+        education: {
+            from: '',
+            to: '',
+            university: '',
+            city: '',
+            degree: '',
+            subject: ''
+        },
+        educations: [
             {
-                id: 0,
                 from: 2019,
                 to: 2021,
-                university: 'Xavier Academy, Kathmandu',
+                university: 'Xavier Academy',
+                city: 'Kathmandu',
                 degree: 'High School',
                 subject: 'Science'
             },
             {
-                id: 1,
                 from: 2023,
-                to: 2027,
-                university: 'San Fransisco Bay University, Fremont',
+                to: 'Present',
+                university: 'San Fransisco Bay University',
+                city: 'Fremont',
                 degree: 'Bachelors degree',
                 subject: 'Computer Science'
             }
         ]
+    } 
 
-        const educationElements = educations.map(education => {
-            return <EducationCard key={education.id} data={education} />
+    delete = (e) => {
+        const universityAndCity = e.target.parentElement.parentElement.parentElement.getElementsByClassName('date-details-container')[0].children[1].getElementsByTagName('h3')[0].innerHTML;
+        const university = universityAndCity.split(',')[0];
+
+        this.setState(prevState => {
+            const oldState = prevState.educations;
+            const newState = oldState.filter(function(eachItem) {
+                if (eachItem.university == university) {
+                    return false;
+                } else {
+                    return true;
+                }
+            })
+            
+            return {
+                educations: [...newState]
+            }
+        })
+    }
+
+    changeEditingMode = () => {
+        this.setState(prevState => {
+            return {
+                isEditing: !prevState.isEditing
+            }
+        })
+    }
+
+    handleChange = (e) => {
+        const {name, value} = e.target;
+        this.setState(prevState => {
+            return {
+                education: {
+                    ...prevState.education,
+                    [name]: value
+                }
+            }
+        })
+    }
+
+    handleFormSubmit = (e) => {
+        e.preventDefault();
+        this.setState(prevState => {
+            return {
+                educations: [...prevState.educations, this.state.education],
+                education: {
+                    from: '',
+                    to: '',
+                    university: '',
+                    city: '',
+                    degree: '',
+                    subject: ''
+                }
+            }
+        })
+        this.changeEditingMode();
+    }
+
+    render() {
+        console.log(this.state);
+
+        const educationElements = this.state.educations.map((education, index) => {
+            return <EducationCard key={index} data={education} deleteFunc={this.delete}/>
         })
 
         return (
             <div className="education-container">
-                <hr></hr>
-                <h1>EDUCATION</h1>
-                <hr></hr>
-                {educationElements}
+                {
+                    this.state.isEditing ? 
+                    <div>
+                        <hr></hr>
+                        <div className="education-heading">
+                            <h1>EDUCATION</h1>
+                            <MdCancel className="cancel-button" onClick={this.changeEditingMode}/>
+                        </div>
+                        <hr className="h"></hr>
+                        <form onSubmit={this.handleFormSubmit}>
+                            <input 
+                                autoComplete="off"
+                                type="text"
+                                placeholder="From"
+                                name="from"
+                                onChange={this.handleChange}
+                            />
+                            <input 
+                                autoComplete="off"
+                                type="text"
+                                placeholder="To"
+                                name="to"
+                                onChange={this.handleChange}
+                            />
+                            <input
+                                autoComplete="off" 
+                                type="text"
+                                placeholder="Name of the University"
+                                name="university"
+                                onChange={this.handleChange}
+                            />
+                            <input 
+                                autoComplete="off"
+                                type="text"
+                                placeholder="City"
+                                name="city"
+                                onChange={this.handleChange}
+                            />
+                            <input 
+                                autoComplete="off"
+                                type="text"
+                                placeholder="Degree"
+                                name="degree"
+                                onChange={this.handleChange}
+                            />
+                            <input 
+                                autoComplete="off"
+                                type="text"
+                                placeholder="Subject"
+                                name="subject"
+                                onChange={this.handleChange}
+                            />
+                            <div>
+                                <button>Add</button>
+                                <button onClick={this.changeEditingMode}>Cancel</button>
+                            </div>
+                        </form>
+                        {educationElements}
+                    </div> :
+                    <div>
+                        <hr></hr>
+                        <div className="education-heading">
+                            <h1>EDUCATION</h1>
+                            <IoMdAddCircle className="add-button" onClick={this.changeEditingMode}/>
+                        </div>
+                        <hr className="h"></hr>
+                        {educationElements}
+                    </div>
+                }
             </div>
         )
     }
--- src/components/RightSide/Education/EducationCard.jsx
+++ src/components/RightSide/Education/EducationCard.jsx
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
-
+import {AiFillDelete} from 'react-icons/ai';
 
 export default class EducationCard extends Component {
     render() {
         return (
             <div className="education-card-container">
-                <div>
-                    <h3>{this.props.data.from} - {this.props.data.to}</h3>
+                <div className="date-details-container">
+                    <div className="date">
+                        <h3>{this.props.data.from} - {this.props.data.to}</h3>
+                    </div>
+                    <div>
+                        <h3>{this.props.data.university}, {this.props.data.city}</h3>
+                        <p>Degree: {this.props.data.degree}</p>
+                        <p>Subject: {this.props.data.subject}</p>
+                    </div>
                 </div>
                 <div>
-                    <h3>{this.props.data.university}</h3>
-                    <p>Degree: {this.props.data.degree}</p>
-                    <p>Subject: {this.props.data.subject}</p>
+                    <AiFillDelete onClick={this.props.deleteFunc}  className="delete-button"/>
                 </div>
             </div>
         )
