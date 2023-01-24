2cd48478cd11aac3ac88e6a2de8919600ab79c71
Aayush99910 <amaharjann01@gmail.com>
2023-01-23 17:22:51 -0800

    user can now edit Profile description

--- src/components/RightSide/Profile/Profile.jsx
+++ src/components/RightSide/Profile/Profile.jsx
@@ -1,12 +1,57 @@
 import React, { Component } from "react";
 import "./Profile.css";
+import {FiEdit} from 'react-icons/fi';
 
 
 export default class Profile extends Component {
+    state = {
+        isEditing: false,
+        description: "Hi I am a Aayush Maharjan. I am originally from Nepal. I really love working with technology. As it grows I am really excited to see more of it."
+    }
+
+    changeEditing = () => {
+        this.setState(prevState => {
+            return {
+                isEditing: !prevState.isEditing
+            }
+        })
+    }
+
+    changeDescription = (event) => {
+        const {name} = event.target;
+        this.setState(prevState => {
+            return {
+                [name]: event.target.value
+            }
+        })
+    }
+
+    submitForm = (e) => {
+        e.preventDefault();
+        this.changeEditing()
+    }
+
+
     render() {
-        return (
+        return ( 
             <div className="profile-container">
-                <p>Hi I am a Aayush Maharjan. I am originally from Nepal. I really love working with technology. As it grows I am really excited to see more of it.</p>
+                {
+                    this.state.isEditing ? 
+                    <div>
+                        <form onSubmit={this.submitForm}>
+                            <textarea 
+                                    name="description" 
+                                    onChange={this.changeDescription} 
+                                    value={this.state.description}
+                            />
+                            <button className="submit-button">Submit</button>
+                        </form>
+                    </div> :
+                    <div>
+                        <p>{this.state.description}</p>
+                        <FiEdit className="edit-button" onClick={this.changeEditing}/>
+                    </div>
+                }
             </div>
         )
     }
